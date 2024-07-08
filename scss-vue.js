7b82db093d00677ebb3068cea5985cfe5f44bcb0
Karl Swedberg <kswedberg@gmail.com>
2024-07-08 14:12:30 -0400

    Fix scss-vue.js

--- scss-vue.js
+++ scss-vue.js
@@ -1,3 +1,8 @@
 export default {
-  extends: ['stylelint-config-recommended-vue/scss', './scss.js'],
+  extends: ['stylelint-config-standard-scss', 'stylelint-config-recommended-vue/scss', './basic.js', './stylistic.js'],
+  rules: {
+    'scss/at-rule-no-unknown': null,
+    'scss/operator-no-unspaced': null,
+    'scss/no-global-function-names': null,
+  },
 };
