e67268551cc578733d71dbdc91e9580974b69689
Karl Swedberg <kswedberg@gmail.com>
2025-04-17 11:46:55 -0400

    Add :deep to ignored pseudo-class selectors in Vue

--- vue.js
+++ vue.js
@@ -1,3 +1,6 @@
 export default {
   extends: ['stylelint-config-standard', 'stylelint-config-recommended-vue', './basic.js', './stylistic.js'],
+  rules: {
+    'selector-pseudo-class-no-unknown': [true, {ignorePseudoClasses: ['input-placeholder', 'deep']}],
+  },
 };
